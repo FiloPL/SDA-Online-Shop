b44fa23106a00ef79e1d0cd5e3004746f931007e
szymon-jedrzejewski <szymon.lukasz.jedrzejewski@gmail.com>
2019-10-27 11:29:48 +0100

    created client class, create setters and getters for fields

---
+++ src/main/java/p16/sdaonlineshop/model/Client.java
@@ -0,0 +1,103 @@
+package p16.sdaonlineshop.model;
+
+import javax.persistence.Entity;
+import java.util.Objects;
+
+@Entity
+public class Client {
+    private String name;
+    private String lastName;
+    private String city;
+    private String street;
+    private String streetNumber;
+    private String postCode;
+    private String email;
+    private String password;
+
+    public Client() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(name, client.name) &&
+                Objects.equals(lastName, client.lastName) &&
+                Objects.equals(city, client.city) &&
+                Objects.equals(street, client.street) &&
+                Objects.equals(streetNumber, client.streetNumber) &&
+                Objects.equals(postCode, client.postCode) &&
+                Objects.equals(email, client.email) &&
+                Objects.equals(password, client.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, lastName, city, street, streetNumber, postCode, email, password);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
