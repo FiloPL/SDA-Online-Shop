fcf5dc008fd60309ecc2e03622cc2e699500dc84
przemyslaw-gosciniak <p.gosciniak@o2.pl>
2019-10-27 12:26:57 +0100

    product-class

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert INTO PRODUCT (id,name,price) value(nextval ('productId'),'Telewizor','1000');
+insert INTO PRODUCT (id,name,price) value(nextval ('productId'),'Pralka','2000');
+insert INTO PRODUCT (id,name,price) value(nextval ('productId'),'Rower','500');
+insert INTO PRODUCT (id,name,price) value(nextval ('productId'),'Odkurzacz','200');
+commit;
