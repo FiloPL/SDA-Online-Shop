096310d725e9eaa7ebe7e48084276e23733f4efa
szymon-jedrzejewski <szymon.lukasz.jedrzejewski@gmail.com>
2019-10-27 12:27:22 +0100

    Added data.sql and create UserController, UserRepository, UserService

---
+++ src/main/java/p16/sdaonlineshop/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package p16.sdaonlineshop.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+}
---
+++ src/main/java/p16/sdaonlineshop/service/UserService.java
@@ -0,0 +1,7 @@
+package p16.sdaonlineshop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
