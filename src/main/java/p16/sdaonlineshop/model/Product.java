36efd71308da78b019b6175e4f704647cbdd1907
przemyslaw-gosciniak <p.gosciniak@o2.pl>
2019-10-27 11:14:33 +0100

    product-class

---
+++ src/main/java/p16/sdaonlineshop/model/Product.java
@@ -0,0 +1,30 @@
+package java.p16.sdaonlineshop.model;
+
+public class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    protected void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Product() {
+    }
+}
