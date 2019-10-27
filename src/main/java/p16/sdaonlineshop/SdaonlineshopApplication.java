e0494afcb76615a83fc60faeb9956dec4db68239
Tomasz Filo Zegarlicki <tzegarlicki@gmail.com>
2019-10-27 10:52:48 +0100

    create

---
+++ src/main/java/p16/sdaonlineshop/SdaonlineshopApplication.java
@@ -0,0 +1,13 @@
+package p16.sdaonlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaonlineshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdaonlineshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/p16/sdaonlineshop/SdaonlineshopApplicationTests.java
@@ -0,0 +1,13 @@
+package p16.sdaonlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdaonlineshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
